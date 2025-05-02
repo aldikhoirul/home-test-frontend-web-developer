552e1d5410122facea5c804dd9d9948e755aa0e4
aldiggy <aldikh@gmail.com>
2025-05-02 20:17:59 +0700

    commit loginform

--- src/app/pages/login/LoginForm.js
+++ src/app/pages/login/LoginForm.js
@@ -151,7 +151,7 @@ export default function LoginForm() {
         )}
 
         <div className="text-center text-sm text-gray-600">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <a
             href="/pages/register"
             className="font-medium text-blue-600 hover:text-blue-500"
