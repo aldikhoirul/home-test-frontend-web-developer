5766a31aec186b254ed56c604e504139ffaeaa11
aldiggy <aldikh@gmail.com>
2025-06-07 11:50:21 +0700

    update sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ export default function Sidebar() {
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("role");
-    router.push("/pages/login");
+    router.push("/");
   };
 
   return (
